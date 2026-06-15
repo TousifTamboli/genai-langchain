e828fbf2ee6b3f2cc9c5a71e7484e5283d83b007
TousifTamboli <tousiftamboli3@gmail.com>
2026-06-16 01:39:54 +0530

    retreiver done

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-import langchain
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
-print(langchain.__version__)
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0.7
+)
+
+result = llm.invoke("Tell me a joke")
+
+print(result.content)
